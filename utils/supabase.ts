3a42a4533f09867811132c5f9fe75a1b04f54e61
TrayMachi <tristan.agra@ui.ac.id>
2025-08-06 04:36:57 +0700

    feat: remove prisma, add supabase

---
+++ utils/supabase.ts
@@ -0,0 +1,19 @@
+import 'react-native-url-polyfill/auto';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.PROJECT_URL;
+const supabaseAnonKey = process.env.ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase URL or anon key');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
