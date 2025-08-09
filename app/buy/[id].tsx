537f923e918006f562062de4a6b5953d36d0964d
TrayMachi <tristan.agra@ui.ac.id>
2025-08-09 19:01:37 +0700

    chore: init buy page

---
+++ app/buy/[id].tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { BuyTicketModule } from '@/modules/BuyTicketModule'
+
+export default function BuyTicketPage() {
+  return <BuyTicketModule />;
+}
---
+++ modules/BuyTicketModule/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const BuyTicketModule = () => {
+  return (
+    <div>BuyTicke</div>
+  )
+}
